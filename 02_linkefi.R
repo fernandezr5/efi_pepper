186c483544a98ae0eb9c30d7688fe0df3e6b025c
Alex Bokov <bokov@uthscsa.edu>
2022-06-24 14:08:15 -0500

    02_linkefi.R; feature; Removes DBI dependency, adds tidyr dependency, dynamic creation of savename (under which to save the output from this script), import and link HBA1c results.

--- 02_linkefi.R
+++ 02_linkefi.R
@@ -1,15 +1,25 @@
 library(dplyr);
-library(DBI);
+library(tidyr);
+#library(DBI);
 library(rio);     # format-agnostic convenient file import
 library(dint);    # date-conversion
 #' The local path names for the data files should be stored in a vector
 #' named `inputdata` that get set in a script named `local_config.R`
 if(file.exists('local_config.R')) source('local_config.R');
+savename <- gsub('[A-Za-z]+_[A-Za-z]+','DEID_EFI',basename(inputdata['samplecsv'])) %>% gsub('.csv.zip','.tsv',.);
 
 #' Import the main data and the EFI mappings
 dat0 <- import(inputdata['samplecsv']);
 efi <- import(inputdata['efixwalk']) %>%
   mutate(patient_num = as.character(patient_num));
-dat1 <- left_join(dat0,efi);
-export(dat1,file='DEID_EFI_HSC20210681E_20220204_9aaf19f0.tsv');
+hba1c <- import(inputdata['hba1c']) %>% mutate(patient_num=as.character(patient_num));
+#' Filter out the patients who don't have valid EFIs within the range of the data
+dat1 <- subset(dat0,patient_num %in% efi$patient_num);
+#' But for the rest, keep all available dates because some of them contain special data elements
+dat2 <- left_join(dat1,efi) %>%
+  left_join(hba1c[,c('patient_num','start_date','medhba1c','vfhba1c')]) %>%
+  fill(medhba1c,vfhba1c,FRAIL6MO,FRAIL12MO,FRAIL24MO) %>%
+  select(!any_of(c('PATIENT_IDE','patient_ide','DATE_SHIFT','date_shift','monthkey')));
+message('Saving data for analysis as ',savename);
+export(dat2,file=savename);
 
